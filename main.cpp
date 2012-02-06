f2d903b435f71536170f0e08fd62cbd112f78fd4
dewillie <edewilliencourt@gmail.com>
2012-02-06 09:35:38 -0800

    PROJET MI PARCOURS

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <QtGui/QApplication>
+#include <QtCore>
+#include <QtGui>
 #include "mainwindow.h"
-#include "controller.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+   QApplication a(argc, argv);
+   MainWindow w;
+   w.show();
+
+
+    //Forme *forme = new Forme();
+    //forme->resize(100, 100);
+    //forme->show();
 
     return a.exec();
 }
