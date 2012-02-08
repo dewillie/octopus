4de2c05d6f172a1b5ba22d43869b66ca648c8255
dewillie <edewilliencourt@gmail.com>
2012-02-08 16:04:17 +0100

    zboub4

--- main.cpp
+++ main.cpp
@@ -13,11 +13,7 @@ int main(int argc, char *argv[])
     //Forme *forme = new Forme();
     //forme->resize(100, 100);
     //forme->show();
-<<<<<<< HEAD
-	//JE PREFERE LES GROS ZBOUB 5
-=======
 	//JE PREFERE LES GROS ZBOUB 4
->>>>>>> zboub4
 	
     return a.exec();
 }
